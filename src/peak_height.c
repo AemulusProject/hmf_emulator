e84421930e916d91a1581be37078d9823d28f2ed
Tom <tom@ITDhelps-MacBook-Pro.local>
2018-11-19 15:01:23 -0500

    Added C files. No way to compile them yet.

---
+++ src/peak_height.c
@@ -0,0 +1,249 @@
+#include "peak_height.h"
+#include "power.h"
+
+#include "gsl/gsl_integration.h"
+#include "gsl/gsl_spline.h"
+
+#include <math.h>
+#include <stdio.h>
+
+#define ABSERR 0.0
+#define RELERR 1e-8
+#define delta_c 1.686 //Critical collapse density
+#define rhocrit 2.77533742639e+11
+//1e4*3.*Mpcperkm*Mpcperkm/(8.*PI*G); units are Msun h^2/Mpc^3
+#define workspace_size 8000
+#define KEY 6 //Used for GSL QAG function
+
+typedef struct integrand_params{
+  gsl_spline *spline;
+  gsl_interp_accel *acc;
+  double r;
+  double*kp; //pointer to wavenumbers
+  double*Pp; //pointer to P(k) array
+  int Nk; //length of k and P arrays
+}integrand_params;
+
+//Redefined these, even though they appear in C_bias.c
+double M_to_R(double M, double Omega_m){
+  //Lagrangian radius Mpc/h
+  return pow(M/(1.33333333333*M_PI*rhocrit*Omega_m),0.3333333333);
+}
+
+double R_to_M(double R, double Omega_m){
+  //Lagrangian mass Msun/h
+  return R*R*R*1.33333333333*M_PI*rhocrit*Omega_m;
+}
+
+double dRdM_at_M(double M, double Omega_m){
+  //Derivative of Lagrangian radius w.r.t. Mass [Msun/h] in Mpc/Msun
+  return pow(36*M_PI*rhocrit*Omega_m,-0.33333333333)*pow(M,-0.6666666666666);
+}
+
+double sigma2_integrand(double lk, void*params){
+  //Integrand for calculating sigma^2
+  integrand_params pars = *(integrand_params*)params;
+  double k = exp(lk);
+  double x = k*pars.r;
+  //double P = get_P(x, pars.r, pars.kp, pars.Pp, pars.Nk, pars.spline, pars.acc);
+  double P = gsl_spline_eval(pars.spline, k, pars.acc);
+  double w = (sin(x)-x*cos(x))*3.0/(x*x*x); //Window function
+  return k*k*k*P*w*w;
+}
+
+double dsigma2dR_integrand(double lk, void*params){
+  //Integrand for calculating dsigma^2/dR, where R is the lagrangian radius
+  integrand_params pars = *(integrand_params*)params;
+  double k = exp(lk);
+  double x = k*pars.r;
+  double P = gsl_spline_eval(pars.spline, k, pars.acc);
+  double sx = sin(x);
+  double cx = cos(x);
+  double w = (sx-x*cx)*3.0/(x*x*x); //Window function
+  double dwdR = k*3*((x*x-3)*sx + 3*x*cx)/(x*x*x*x);
+  return k*k*k*P*w*dwdR;
+}
+
+///////////// linar matter variance functions /////////////
+
+double sigma2_at_R(double R, double*k, double*P, int Nk){
+  //sigma^2(R) for a single value of R
+  double*Rs = (double*)malloc(sizeof(double));
+  double*s2 = (double*)malloc(sizeof(double));
+  double result;
+  Rs[0] = R;
+  sigma2_at_R_arr(Rs, 1, k, P, Nk, s2);
+  result = s2[0];
+  free(Rs);
+  free(s2);
+  return result;
+}
+
+double sigma2_at_M(double M, double*k, double*P, int Nk, double om){
+  //sigma^2(M) for a single value of M
+  double R=M_to_R(M, om);
+  return sigma2_at_R(R, k, P, Nk);
+}
+
+int sigma2_at_R_arr(double*R, int NR,  double*k, double*P, int Nk, double*s2){
+  //sigma^2(R) for an array of R
+  //Initialize GSL things and the integrand structure.
+  gsl_spline*spline = gsl_spline_alloc(gsl_interp_cspline,Nk);
+  gsl_interp_accel*acc = gsl_interp_accel_alloc();
+  gsl_integration_workspace*workspace = gsl_integration_workspace_alloc(workspace_size);
+  gsl_function F;
+  integrand_params*params = (integrand_params*)malloc(sizeof(integrand_params));
+  double lkmin = log(k[0]);
+  double lkmax = log(k[Nk-1]);
+  double result,abserr;
+  double denom_inv = 1./(2*M_PI*M_PI);
+  int i;
+  gsl_spline_init(spline,k,P,Nk);
+  params->spline = spline;
+  params->acc = acc;
+  params->kp = k;
+  params->Pp = P;
+  params->Nk = Nk;
+  F.function = &sigma2_integrand;
+  F.params = params;
+  for(i = 0; i < NR; i++){
+    params->r = R[i];
+    gsl_integration_qag(&F, lkmin, lkmax, ABSERR, RELERR, workspace_size, KEY, workspace, &result, &abserr);
+    s2[i] = result * denom_inv; //divide by 2pi^2
+  }
+  gsl_spline_free(spline);
+  gsl_interp_accel_free(acc);
+  gsl_integration_workspace_free(workspace);
+  free(params);
+  return 0;
+}
+
+int sigma2_at_M_arr(double*M, int NM,  double*k, double*P, int Nk, double om, double*s2){
+  int i;
+  double*R = (double*)malloc(sizeof(double)*NM);
+  for(i = 0; i < NM; i++){
+    R[i] = M_to_R(M[i], om);
+  }
+  sigma2_at_R_arr(R, NM, k, P, Nk, s2);
+  free(R);
+  return 0;
+}
+
+/* The derivative with respect to R of sigma^2. This is needed for the mass
+ * function and replaces having to take a numerical derivative.
+ */
+int dsigma2dR_at_R_arr(double*R, int NR, double*k, double*P, int Nk, double*ds2dR){
+  //Initialize GSL things and the integrand structure.
+  gsl_spline*spline = gsl_spline_alloc(gsl_interp_cspline,Nk);
+  gsl_interp_accel*acc = gsl_interp_accel_alloc();
+  gsl_integration_workspace*workspace = gsl_integration_workspace_alloc(workspace_size);
+  gsl_function F;
+  integrand_params*params = (integrand_params*)malloc(sizeof(integrand_params));
+  double lkmin = log(k[0]);
+  double lkmax = log(k[Nk-1]);
+  double result,abserr;
+  double denom_inv = 1./(2*M_PI*M_PI);
+  int i;
+  gsl_spline_init(spline,k,P,Nk);
+  params->spline = spline;
+  params->acc = acc;
+  params->kp = k;
+  params->Pp = P;
+  params->Nk = Nk;
+  F.function = &dsigma2dR_integrand;
+  F.params = params;
+  for(i = 0; i < NR; i++){
+    params->r = R[i];
+    gsl_integration_qag(&F, lkmin, lkmax, ABSERR, RELERR, workspace_size, KEY, workspace, &result, &abserr);
+    ds2dR[i] = result * denom_inv; //divide by 2pi^2
+  }
+  gsl_spline_free(spline);
+  gsl_interp_accel_free(acc);
+  gsl_integration_workspace_free(workspace);
+  free(params);
+  return 0;
+  return 0;
+}
+
+double dsigma2dR_at_R(double R, double*k, double*P, int Nk){
+  //sigma^2(R) for a single value of R
+  double*Rs = (double*)malloc(sizeof(double));
+  double*ds2dR = (double*)malloc(sizeof(double));
+  double result;
+  Rs[0] = R;
+  dsigma2dR_at_R_arr(Rs, 1, k, P, Nk, ds2dR);
+  result = ds2dR[0];
+  free(Rs);
+  free(ds2dR);
+  return result;
+}
+
+/* The derivative with respect to M of sigma^2. This is needed for the mass
+ * function and replaces having to take a numerical derivative.
+ */
+int dsigma2dM_at_M_arr(double*M, int NM, double*k, double*P, int Nk, double Omega_m, double*ds2dM){
+  int i;
+  double*R = (double*)malloc(sizeof(double)*NM);
+  double*dRdM = (double*)malloc(sizeof(double)*NM);
+  for(i = 0; i < NM; i++){
+    R[i] = M_to_R(M[i], Omega_m);
+    dRdM[i] = dRdM_at_M(M[i], Omega_m);
+  }
+  dsigma2dR_at_R_arr(R, NM, k, P, Nk, ds2dM);
+  //Chain rule to convert dsigma2dR to dsigma2dM
+  for(i = 0; i < NM; i++){
+    ds2dM[i] = ds2dM[i] * dRdM[i];
+  }
+  free(R);
+  free(dRdM);
+  return 0;
+}
+
+double dsigma2dM_at_M(double M, double*k, double*P, int Nk, double Omega_m){
+  double*Ms = (double*)malloc(sizeof(double));
+  double*ds2dM = (double*)malloc(sizeof(double));
+  double result;
+  Ms[0] = M;
+  dsigma2dR_at_R_arr(Ms, 1, k, P, Nk, ds2dM);
+  result = ds2dM[0];
+  free(Ms);
+  free(ds2dM);
+  return result;
+}
+
+///////////PEAK HEIGHT FUNCTIONS///////////
+
+double nu_at_R(double R, double*k, double*P, int Nk){
+  //peak height at R
+  return delta_c/sqrt(sigma2_at_R(R, k, P, Nk));
+}
+
+double nu_at_M(double M, double*k, double*P, int Nk, double om){
+  //peak height at M
+  double R=M_to_R(M, om);
+  return nu_at_R(R, k, P, Nk);
+}
+
+int nu_at_R_arr(double*R, int NR, double*k, double*P, int Nk, double*nu){
+  //peak height at an array of R
+  int i;
+  double*s2 = (double*)malloc(sizeof(double)*NR);
+  sigma2_at_R_arr(R, NR, k, P, Nk, s2);
+  for(i = 0; i < NR; i++){
+    nu[i] = delta_c/sqrt(s2[i]);
+  }
+  free(s2);
+  return 0;
+}
+
+int nu_at_M_arr(double*M, int NM, double*k, double*P, int Nk, double om, double*nu){
+  //peak height at an array of M
+  int i;
+  double*R = (double*)malloc(sizeof(double)*NM);
+  for(i = 0; i < NM; i++){
+    R[i] = M_to_R(M[i], om);
+  }
+  nu_at_R_arr(R, NM, k, P, Nk, nu);
+  free(R);
+  return 0;
+}
---
+++ src/power.c
@@ -0,0 +1,40 @@
+/** @file power.c
+ *  @brief Function for integrands that require P(x/R).
+ *
+ * This file contains a function to access the power
+ * spectrum P(k) in integrands that actually evaluate
+ * P(x/R) where x=kR.
+ * 
+ * @author Tom McClintock
+ */
+
+#include "gsl/gsl_spline.h"
+#include <math.h>
+
+/**
+ * \brief Evaluate the power spectrum P(x/R).
+ *
+ * Certain integrals require evaluating the power spectrum at P(k=x/R).
+ * This function takes in a spline for a pre-computed power spectrum P(k)
+ * and returns the evaluation of P(k) either within the region that
+ * the spline is valid or with power law approximations at smaller or
+ * larger scales.
+ */
+double get_P(double x, double R, double*k, double*P, int Nk, void*void_Pspl, void*void_acc){
+  gsl_spline*Pspl = (gsl_spline*)void_Pspl;
+  gsl_interp_accel*acc = (gsl_interp_accel*)void_acc;
+  double ki = x/R;
+  double kmin = k[0];
+  double kmax = k[Nk-1];
+  double alpha,A;
+  if (ki < kmin){
+    alpha = log(P[1]/P[0])/log(k[1]/k[0]);
+    A = P[0]/pow(k[0],alpha);
+    return A*pow(ki,alpha);
+  }else if (ki > kmax){
+    alpha = log(P[Nk-1]/P[Nk-2])/log(k[Nk-1]/k[Nk-2]);
+    A = P[Nk-1]/pow(k[Nk-1],alpha);
+    return A*pow(ki,alpha);
+  }// Assume power laws at ends
+  return gsl_spline_eval(Pspl,ki,acc);
+}
