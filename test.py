6ce1ddb5e17639d228df174f090bb1bf34a272c2
Tom McClintock <tmcclintock89@gmail.com>
2018-12-05 13:37:23 -0500

    Wrote passing functions for everything that needs defining in order to subclass Aemulator.

--- test.py
+++ test.py
@@ -2,12 +2,30 @@
 
 from Aemulator import *
 
-class myobj(Aemulator):
+class hmf_emulator(Aemulator):
 
     def __init(self):
         Aemulator.__init__(self)
         print("i exist")
 
+    def load_data(self, filename):
+        pass
+
+    def build_emulator(self, hyperparams=None):
+        pass
+
+    def train_emulator(self):
+        pass
+
+    def cache_emulator(self, filename):
+        pass
+
+    def load_emulator(self, filename):
+        pass
+
+    def predict(self, params):
+        pass
+
 if __name__=="__main__":
-    m = myobj()
-    print(m)
+    e = hmf_emulator()
+    print(e)
