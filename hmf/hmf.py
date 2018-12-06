4a2b8f69966f9773d722375a9c9b0ab0ac3090b8
Tom McClintock <tmcclintock89@gmail.com>
2018-12-05 22:20:32 -0500

    Building and training is complete.

--- hmf/hmf.py
+++ hmf/hmf.py
@@ -3,13 +3,20 @@
 from Aemulator import *
 import numpy as np
 import os, inspect
+import scipy.optimize as op
+import george
+from george.kernels import *
 
 class hmf_emulator(Aemulator):
 
     def __init__(self):
         Aemulator.__init__(self)
         self.loaded_data = False
+        self.built       = False
+        self.trained     = False
         self.load_data()
+        self.build_emulator()
+        self.train_emulator()
 
     def load_data(self, path_to_training_data_directory = None):
         """
@@ -30,6 +37,7 @@ class hmf_emulator(Aemulator):
         #Load all training data
         self.training_cosmologies = \
             np.load(data_path+"training_cosmologies.npy")
+        self.N_cosmological_params = len(self.training_cosmologies[0])
         self.rotation_matrix      = \
             np.load(data_path+"rotation_matrix.npy")
         self.training_data        = \
@@ -50,11 +58,50 @@ class hmf_emulator(Aemulator):
             None
         """
         if not self.loaded_data:
-            raise Exception("Need to load training data first.")
-        pass
+            raise Exception("Need to load training data before building.")
+
+        if hyperparams is None:
+            hyperparams = np.std(self.training_cosmologies, 0)
+
+        N_cosmological_params = self.N_cosmological_params
+        means  = self.training_mean
+        stddev = self.training_stddev
+
+        #Assemble the list of GPs
+        self.N_GPs = len(means[0])
+        self.GP_list = []
+        for i in range(self.N_GPs):
+            y    = means[:, i]
+            ystd = stddev[:, i]
+            kernel = ExpSquaredKernel(hyperparams, ndim=N_cosmological_params)
+            gp = george.GP(kernel, mean=np.mean(y),
+                           white_noise=np.log(np.mean(ystd)**2))
+            gp.compute(self.training_cosmologies, ystd)
+            self.GP_list.append(gp)
+            continue
+        self.built = True
+        return
 
     def train_emulator(self):
-        pass
+        if not self.built:
+            raise Exception("Need to build before training.")
+
+        means  = self.training_mean
+
+        for i, gp in enumerate(self.GP_list):
+            y    = means[:, i]
+            def nll(p):
+                gp.set_parameter_vector(p)
+                ll = gp.lnlikelihood(y, quiet=True)
+                return -ll if np.isfinite(ll) else 1e25
+            def grad_nll(p):
+                gp.set_parameter_vector(p)
+                return -gp.grad_lnlikelihood(y, quiet=True)
+            p0 = gp.get_parameter_vector()
+            result = op.minimize(nll, p0, jac=grad_nll)
+            gp.set_parameter_vector(result.x)
+        self.trained = True
+        return
 
     def cache_emulator(self, filename):
         pass
--- tests/test_hmf.py
+++ tests/test_hmf.py
@@ -3,11 +3,28 @@ from hmf import *
 import numpy as np
 import numpy.testing as npt
 
+#Create an emulator
+h = hmf.hmf_emulator()
+
 def test_hmf_emulator_load_data():
-    h = hmf.hmf_emulator()
     npt.assert_equal(h.loaded_data, True)
     attrs = ["data_path", "training_cosmologies",
              "rotation_matrix", "training_data",
              "training_mean", "training_stddev"]
     for attr in attrs:
         npt.assert_equal(hasattr(h, attr), True)
+        continue
+    return
+
+def test_hmf_emulator_build_emulator():
+    npt.assert_equal(h.built, True)
+    attrs = ["N_GPs", "GP_list"]
+    for attr in attrs:
+        npt.assert_equal(hasattr(h, attr), True)
+        continue
+    npt.assert_equal(h.N_GPs, len(h.GP_list))
+    return
+
+def test_hmf_emulator_train_emulator():
+    npt.assert_equal(h.trained, True)
+    return
