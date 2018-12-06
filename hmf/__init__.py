9d37ebc3ad6a157479ce3dec2c1612f7f043b910
tmcclintock <tmcclintock89@gmail.com>
2018-12-06 11:51:26 -0500

    Wrote all the dndM functions. Need to test and make examples.

--- hmf/__init__.py
+++ hmf/__init__.py
@@ -1,23 +1,4 @@
-from hmf import *
-import cffi, glob, os
+from .hmf import *
 
 __author__ = "Tom McClintock <mcclintock@bnl.gov>"
 
-hmf_dir = os.path.dirname(__file__)
-include_dir = os.path.join(hmf_dir,'src')
-lib_file    = os.path.join(hmf_dir,'_hmf.so')
-# Some installation (e.g. Travis with python 3.x)
-# name this e.g. _hmf.cpython-34m.so,
-# so if the normal name doesn't exist, look for something else.
-if not os.path.exists(lib_file):
-    alt_files = glob.glob(os.path.join(os.path.dirname(__file__),'_hmf*.so'))
-    if len(alt_files) == 0:
-        raise IOError("No file '_hmf.so' found in %s"%hmf_dir)
-    if len(alt_files) > 1:
-        raise IOError("Multiple files '_hmf*.so' found in %s: %s"%(hmf_dir,alt_files))
-    lib_file = alt_files[0]
-
-_ffi = cffi.FFI()
-for file_name in glob.glob(os.path.join(include_dir,'*.h')):
-    _ffi.cdef(open(file_name).read())
-_lib = _ffi.dlopen(lib_file)
--- hmf/hmf.py
+++ hmf/hmf.py
@@ -2,10 +2,35 @@
 
 from Aemulator import *
 import numpy as np
-import os, inspect
+import cffi, glob, os, inspect, pickle, warnings
 import scipy.optimize as op
+from scipy.interpolate import InterpolatedUnivariateSpline as IUS
 import george
-import pickle
+from classy import Class
+
+#Create the CFFI library
+hmf_dir = os.path.dirname(__file__)
+include_dir = os.path.join(hmf_dir,'src')
+lib_file    = os.path.join(hmf_dir,'_hmf.so')
+# Some installation (e.g. Travis with python 3.x)
+# name this e.g. _hmf.cpython-34m.so,
+# so if the normal name doesn't exist, look for something else.
+if not os.path.exists(lib_file):
+    alt_files = glob.glob(os.path.join(os.path.dirname(__file__),'_hmf*.so'))
+    if len(alt_files) == 0:
+        raise IOError("No file '_hmf.so' found in %s"%hmf_dir)
+    if len(alt_files) > 1:
+        raise IOError("Multiple files '_hmf*.so' found in %s: %s"%(hmf_dir,alt_files))
+    lib_file = alt_files[0]
+_ffi = cffi.FFI()
+for file_name in glob.glob(os.path.join(include_dir,'*.h')):
+    _ffi.cdef(open(file_name).read())
+_lib = _ffi.dlopen(lib_file)
+
+#Used to case things correctly
+def _dc(x):
+    if isinstance(x, list): x = np.asarray(x, dtype=np.float64, order='C')
+    return _ffi.cast('double*', x.ctypes.data)
 
 class hmf_emulator(Aemulator):
 
@@ -17,6 +42,7 @@ class hmf_emulator(Aemulator):
         self.load_data()
         self.build_emulator()
         self.train_emulator()
+        self.cosmology_is_set = False
 
     def load_data(self, path_to_training_data_directory = None):
         """
@@ -111,6 +137,8 @@ class hmf_emulator(Aemulator):
     def cache_emulator(self, filename):
         """
         Cache the emulator to a file for easier re-loadig. 
+        Note: this function and load_emulator() do not work correctly,
+        because many attributes are not pickle-able.
         :param filename:
             The filename where the trained emulator will be cached.
         :return:
@@ -123,6 +151,8 @@ class hmf_emulator(Aemulator):
     def load_emulator(self, filename):
         """
         Load an emulator directly from file, pre-trained.
+        Note: this function and cache_emulator() do not work correctly,
+        because many attributes are not pickle-able.
         :param filename:
             The filename where the trained emulator is located, in a format compatible with
             this object.
@@ -143,6 +173,7 @@ class hmf_emulator(Aemulator):
     def predict(self, params):
         """
         Use the emulator to make a prediction at a point in parameter space.
+        Note: this returns the slopes and intercepts for the fit function.
         :param params:
             A dictionary of parameters, where the key is the parameter name and
             value is its value.
@@ -150,8 +181,156 @@ class hmf_emulator(Aemulator):
             pred, the emulator prediction at params. Will be a float or numpy array,
             depending on the quantity being emulated.
         """
-        pass
+        if not self.trained:
+            raise Exception("Need to train the emulator first.")
 
+        #Properly organize the data
+        cos_arr = np.zeros(7)
+        cos_arr[0] = params['omega_b'] #Omega_b*h^2
+        cos_arr[1] = params['omega_cdm'] #Omega_cdm*h^2
+        cos_arr[2] = params['w0']
+        cos_arr[3] = params['n_s']
+        cos_arr[4] = params['ln10As']
+        cos_arr[5] = params['H0']
+        cos_arr[6] = params['N_eff']
+        cos_arr = np.atleast_2d(cos_arr)
+
+        means = self.means.T #Transpose of mean data
+        output = np.array([gp.predict(y, cos_arr) for y,gp in zip(means, self.GP_list)])
+        return np.dot(self.rotation_matrix, output).flatten()
+
+    def set_cosmology(self, params):
+        """
+        Set the cosmological parameters of the emulator. One must
+        call this function before actually computing mass functions.
+        :param params:
+            A dictionary of parameters, where the key is the parameter name and
+            value is its value.
+        :return:
+            None
+        """
+        self.mf_slopes_and_intercepts = self.predict(params)
+        #Set up a CLASS dictionary
+        self.h = params['H0']/100.
+        self.Omega_m = (params["omega_b"]+params["omega_cdm"])/self.h**2
+        class_cosmology = {
+            'output': 'mPk',
+            'H0':           params['H0'],
+            'ln10^{10}A_s': params['ln10As'],
+            'n_s':          params['n_s'],
+            'w0_fld':       params['w0'],
+            'wa_fld':       0.0,
+            'omega_b':      params['omega_b'],
+            'omega_cdm':    params['omega_cdm'],
+            'Omega_Lambda': 1 - self.Omega_m,
+            'N_eff':        params['N_eff'],
+            'P_k_max_1/Mpc': 10.,
+            'z_max_pk':      5.03
+        }
+        #Seed splines in CLASS
+        cc = Class()
+        cc.set(class_cosmology)
+        cc.compute()
+        #Make everything attributes
+        self.cc = cc
+        self.k = np.logspace(-5, 1, num=1000) # Mpc^-1 comoving
+        self.M = np.logspace(10, 16.5, num=1000) # Msun/h
+        self.computed_sigma2    = {}
+        self.computed_dsigma2dM = {}
+        self.computed_pk        = {}
+        self.cosmology_is_set = True
+        return
+
+    def _compute_massfunction_parameters(self, redshifts):
+        if not self.cosmology_is_set:
+            raise Exception("Must set_cosmology() first.")
+        e0,f0,g0,d1,e1,g1 = self.mf_slopes_and_intercepts
+        d0, f1 = [ 2.39279115, 0.11628991]
+        x = 1./(1+redshifts)-0.5
+        d = d0 + x*d1
+        e = e0 + x*e1
+        f = f0 + x*f1
+        g = g0 + x*g1
+        return np.array([d, e, f, g]).flatten()
+
+    def _compute_sigma(self, redshifts):
+        if not self.cosmology_is_set:
+            raise Exception("Must set_cosmology() first.")
+        redshifts = np.array(redshifts)
+        if redshifts.ndim > 1:
+            raise Exception("Redshifts be either a scalar or 1D array.")
+        h, Omega_M = self.h, self.Omega_m #Hubble constant and matter fraction
+        k, M = self.k, self.M #wavenumbers and halo masses
+        kh = k/h #h/Mpc
+        for i,z in enumerate(np.atleast_1d(redshifts)):
+            if z in self.computed_sigma2.keys:
+                continue
+            p = np.array([self.cc.pk_lin(ki, z) for ki in k])*h**3 #[Mpc/h]^3
+            self.computed_sigma2[z]    = _lib.sigma2_at_M(_dc(M), _dc(kh), _dc(p), Omega_m)
+            self.computed_dsigma2dM[z] = _lib.dsigma2dM_at_M(_dc(M), _dc(kh), _dc(p), Omega_m)
+            self.computed_pk[z] = p
+            continue
+        return
+
+    def dndM(self, Masses, redshifts):
+        if not self.cosmology_is_set:
+            raise Exception("Must set_cosmology() first.")
+        Masses = np.array(Masses, order='C')
+        if Masses.ndim >1:
+            raise Exception("Masses must be either scalar or 1D array.")
+        if any(Masses < 1e10) or any(Masses > 16.5):
+            raise Exception("Mass outside of range 1e10-1e16.5 Msun/h.")
+        if any(redshifts > 5):
+            raise Exception("Redshift greater than 5.")
+        if any(redshifts > 3):
+            print("Warning: redshift greather than 3. Accuracy not guaranteed.")
+
+        self._compute_sigma(self, redshifts)
+        Omega_m = self.Omega_m
+        lnM = np.log(Masses)
+        NM = len(np.atleast_1d(Masses))
+        Nz = len(np.atleast_1d(redshifts))
+        dndM_out = np.zeros((Nz, NM))
+        output = _dc(np.zeros_like(Masses))
+        for i,z in enumerate(np.atleast_1d(redshifts)):
+            d,e,f,g = self._compute_massfunction_parameters(z)
+            lnsigma2_spline = IUS(np.log(self.M), np.log(self.computed_sigma2))
+            lndsigma2dM_spline = IUS(np.log(self.M), np.log(self.computed_dsigma2dM))
+            sigma2    = np.exp(lnsigma2_spline(lnM))
+            dsigma2dM = np.exp(lndsigma2dM_spline(lnM))
+            _lib.dndM_sigma2_precomputed(_dc(Masses), _dc(sigma2), _dc(dsigma2dM), NM, Omega_m, d, e, f, g, output)
+            dndM_out[i] = out
+            continue
+        return dndM_out
+
+    def n_in_bins(self, Mass_bin_edges, redshifts):
+        Mass_bin_edges = np.array(Mass_bin_edges, order='C')
+        if Mass_bin_edges.ndim > 1:
+            raise Exception("Mass bin edges must be a 1D array.")
+        if any(redshifts > 5):
+            raise Exception("Redshift greater than 5.")
+        if any(redshifts > 3):
+            print("Warning: redshift greather than 3. Accuracy not guaranteed.")
+
+        Nz = len(np.atleast_1d(redshifts))
+        output = np.zeros((Nz, len(Mass_bin_edges)-1))
+        M = self.M
+        NM = len(M)
+        dndM   = _dc(np.zeros_like(M))
+        n_bins = _dc(np.zeros(len(Mass_bin_edges)-1))
+        for i,z in enumerate(np.atleast_1d(redshifts)):
+            d,e,f,g = self._compute_massfunction_parameters(z)
+            self._compute_sigma(z)
+            sigma2    = self.computed_sigma2[z]
+            dsigma2dM = self.computed_dsigma2dM[z]
+            _lib.dndM_sigma2_precomputed(_dc(N), _dc(sigma2), _dc(dsigma2dM), NM, Omega_m, d, e, f, g, dndM)
+            _lib.n_in_bins(_dc(Mass_bin_edges), len(Mass_bin_edges), _dc(M), dndM, NM, n_bins)
+            output[i] = n_bins
+            continue
+        if Nz == 1:
+            return output.flatten()
+        return output
+            
 if __name__=="__main__":
     e = hmf_emulator()
     print(e)
