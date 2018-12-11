35a6b97ad3e1f96c190faeb9f58d21302a73e080
Tom McClintock <tmcclintock89@gmail.com>
2018-12-11 13:18:33 -0500

    Moved header files to include/

--- hmf/hmf.py
+++ hmf/hmf.py
@@ -10,7 +10,7 @@ from classy import Class
 
 #Create the CFFI library
 hmf_dir = os.path.dirname(__file__)
-include_dir = os.path.join(hmf_dir,'src')
+include_dir = os.path.join(hmf_dir,'include')
 lib_file    = os.path.join(hmf_dir,'_hmf.so')
 # Some installation (e.g. Travis with python 3.x)
 # name this e.g. _hmf.cpython-34m.so,
--- setup.py
+++ setup.py
@@ -4,10 +4,10 @@ import subprocess
 
 # Make a symlink between the hmf directory and the src directory
 # in order to see the .h files.
-os.system('ln -s ../src hmf/src')
+os.system('ln -s ../inlcude hmf/include')
 
 sources = glob.glob(os.path.join('src','*.c'))
-headers = glob.glob(os.path.join('src','*.h'))
+headers = glob.glob(os.path.join('include','*.h'))
 try:
     cflags = subprocess.check_output(['gsl-config', '--cflags'], universal_newlines=True).split()
     lflags = subprocess.check_output(['gsl-config', '--libs'], universal_newlines=True).split()
@@ -17,7 +17,7 @@ except OSError:
 ext=Extension("hmf._hmf",
               sources,
               depends=headers,
-              include_dirs=['src'],
+              include_dirs=['include'],
               extra_compile_args=[os.path.expandvars(flag) for flag in cflags],
               extra_link_args=[os.path.expandvars(flag) for flag in lflags])
 
