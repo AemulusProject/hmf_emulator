67c2fbd2494d747929a92630ba4ffc02fe7a752b
Tom McClintock <tmcclintock89@gmail.com>
2018-12-13 13:30:54 -0500

    Fixed a typo in the setup.py file.

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ import subprocess
 
 # Make a symlink between the hmf directory and the src directory
 # in order to see the .h files.
-os.system('ln -s ../inlcude hmf/include')
+os.system('ln -s ../include hmf/include')
 
 sources = glob.glob(os.path.join('src','*.c'))
 headers = glob.glob(os.path.join('include','*.h'))
